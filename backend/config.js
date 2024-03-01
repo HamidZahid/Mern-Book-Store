8c06665ec50b0dc722a34cbe8f2d5a6c13e0c3d4
HamidZahid <hamidchaudhary3867@gmail.com>
2024-03-01 16:35:39 +0500

    Push

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://admin:admin@mernbookstore.w3espjn.mongodb.net/Mern-Stack-Book-Store?retryWrites=true&w=majority&appName=MernBookStore";
